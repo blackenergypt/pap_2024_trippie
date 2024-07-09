839538d7084b2637e0009129cd805585ce26acfd
blackenergypt <51200836+blackenergypt@users.noreply.github.com>
2024-07-09 06:34:04 +0100

    remove libs

--- admin/admin_dashboard.php
+++ admin/admin_dashboard.php
@@ -21,7 +21,6 @@ require '../config.php';
     <link rel="icon" href="../assets/images/lock.png" type="image/png">
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">>
-    <link href="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/style.min.css" rel="stylesheet" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/11.0.18/sweetalert2.min.css">
     <link href="css/styles.css" rel="stylesheet" />
 </head>
